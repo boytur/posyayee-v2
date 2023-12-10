ec986270baa3f121e5e3fd9513143085c6e8bee4
Piyawat Wongyat <mask30254@gmail.com>
2023-12-10 13:00:56 +0700

    feat:(App): implement routing for home, price, documentation, reviews, and contact pages
    - Integrated React Router for navigation between different sections of the application
    - Created routes for the Home, Price, Documentation, Reviews, and Contact components
    
    # Components/Files Affected:
    - App.jsx
    
    # Dependencies:
    - React Router (react-router-dom)
    - Home, Price, Documentation, Review, and Contact components
    
    # Additional Notes:
    - The App component now utilizes React Router for client-side navigation
    - Routes are set up for the Home, Price, Documentation, Reviews, and Contact pages
    
    # Issue(s) Closed:
    - None
    
    # Issue(s) Resolved:
    - None
    
    # Test Plan:
    - Manually tested each route to ensure proper navigation and component rendering

--- posyayee-home/src/App.jsx
+++ posyayee-home/src/App.jsx
@@ -1,10 +1,24 @@
+import Documentation from "./components/Documentation/Documentation";
+import Home from "./components/Home/Home";
+import Price from "./components/Price/Price";
+import Contact from "./components/Contact/Contact";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Review from "./components/Review/Review";
 
 function App() {
   return (
     <>
-    <h1 className=" text-amber-600">setup tailwind css</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/price" element={<Price />} />
+          <Route path="/documentation" element={<Documentation />} />
+          <Route path="/reviews" element={<Review />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
