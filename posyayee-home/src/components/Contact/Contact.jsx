828ac2a5fb016aa5e2e124a59710a6ff6ad42f10
Piyawat Wongyat <mask30254@gmail.com>
2023-12-10 14:25:45 +0700

    setup: add alternative routes and packages to be used in developing new features

---
+++ posyayee-home/src/components/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Navbar/Navbar"
+
+function Contact() {
+  return (
+    <div>
+      <Navbar/>
+      <p>contact</p>
+    </div>
+  )
+}
+
+export default Contact
---
+++ posyayee-home/src/components/Price/Price.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Navbar/Navbar"
+
+function Price() {
+  return (
+    <div>
+      <Navbar/>
+      <p>Price</p>
+    </div>
+  )
+}
+
+export default Price
