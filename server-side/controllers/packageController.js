54eedead398d5fbf2ebc2a058335ae92f59b878e
Piyawat Wongyat <104257779+boytur@users.noreply.github.com>
2024-01-21 04:30:10 +0700

    refactor: package controller

--- server-side/controllers/packageController.js
+++ server-side/controllers/packageController.js
@@ -5,9 +5,9 @@ const PackageModel = require('../models/PackageModel');
 //Add new packages
 app.post('/api/package/add-package', async (req, res) => {
     try {
-        const { pack_price, pack_name } = req.body;
+        const { package_price, package_name } = req.body;
         switch (true) {
-            case pack_price != 0 && !pack_price || !pack_name:
+            case package_price != 0 && !package_price || !package_name:
                 res.status(500).send({ msg: "กรุณาใส่ข้อมูลให้ครบถ้วนค่ะ!" });
                 return;
         }
